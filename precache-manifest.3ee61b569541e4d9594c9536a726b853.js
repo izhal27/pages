d3ee7f682cade2cce2500b04879a7f9ee5563417
Risal Walangadi <risal.walangadi@gmail.com>
2019-07-12 14:45:43 +0800

    Updates

---
+++ precache-manifest.3ee61b569541e4d9594c9536a726b853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24be34e87a952a60aa06bd2287143dc",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "70ec47e85542650053fd",
+    "url": "/pages/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "70c102b682ff02c18b16",
+    "url": "/pages/static/css/main.b8218649.chunk.css"
+  },
+  {
+    "revision": "70ec47e85542650053fd",
+    "url": "/pages/static/js/2.eb24456f.chunk.js"
+  },
+  {
+    "revision": "70c102b682ff02c18b16",
+    "url": "/pages/static/js/main.3b2f7601.chunk.js"
+  },
+  {
+    "revision": "cf56c44899a2ed10ff78",
+    "url": "/pages/static/js/runtime~main.5e0a9daa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
